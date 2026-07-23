bf8206e087f5ec9b58ad103570aebbe3a08b3024
lalamojaden <lalamojaden@gmail.com>
2026-07-23 13:06:25 +0800

    Create KawaifuNotifier.lua

---
+++ KawaifuNotifier.lua
@@ -0,0 +1,11 @@
+-- Roblox Instant Leave
+
+-- For your own game / authorized testing
+
+
+
+local Players = game:GetService("Players")
+
+
+
+Players.LocalPlayer:Kick("your account get banned for 10 days (for cheating) ")
